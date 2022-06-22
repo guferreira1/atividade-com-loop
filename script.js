73efdf9a89982871e80fee5415d3d6cdb7a8b7e5
guferreira1 <gutoconsultor.imoveis@hotmail.com>
2022-06-22 14:33:01 -0300

    correção dos arrays

--- script.js
+++ script.js
@@ -1,108 +1,128 @@
 function oneThroughTwenty(num1a20) {
-    for (let i = 1; i <= num1a20; i++){
-      console.log(i)
-    }
+  let arrRetorno = []
+  for (let i = 1; i <= num1a20; i++){
+    arrRetorno.push(i)
   }
-  oneThroughTwenty(20)
-  
-  
-  
-  function evensToTwenty(numPar) {
-      for (let i = 1; i <= numPar; i++) {
-        if (i % 2 === 0){
-          console.log(i)
-        }
-      }
-  }
-  evensToTwenty(20)
-  
-  
-  
-  
-  function oddsToTwenty(numImpar) {
-      
-    for (let i = 1; i <= numImpar; i++) {
-      if (i % 2 !== 0){
-        console.log(i)
-      }
-    }
-  }
-  oddsToTwenty(20)
-  
-  
-  
-  
-  function multiplesOfFive(multiplosDe5) {
-      for (i = 5; i <= multiplosDe5; i++) {
-          if (i % 5 == 0){
-          console.log(i);
-        }
-      }
-    }
-  multiplesOfFive(100)
-  
-  
-  function squareNumbers(numQuadrado) {
-      for (let i = 1; i <= numQuadrado; i++) {
-        if (Math.sqrt(i) % 1 === 0) {
-          console.log(i)
-        }
-      }
-  }
-  squareNumbers(100)
-  
-  
-  function countingBackwards(num20a1) {
-      
-    for (let i = 20; i >= num20a1; i--){
-      console.log(i)
-    }
-  }
-  countingBackwards(1)
-  
-  
-  
-  function evenNumbersBackwards(parInvertido) {
-    for (let i = 20; i >= parInvertido; i--) {
+  return arrRetorno
+}
+oneThroughTwenty(20)
+console.log(oneThroughTwenty(20))
+
+
+function evensToTwenty(numPar) {
+  let retornoPar = []
+    for (let i = 1; i <= numPar; i++) {
       if (i % 2 === 0){
-        console.log(i)
+        retornoPar.push(i)
       }
     }
-  }
-  evenNumbersBackwards(1)
-  
-  
-  
-  function oddNumbersBackwards(imparInvertido) {
-      
-    for (let i = 20; i >= imparInvertido; i--) {
-      if (i % 2 !== 0){
-        console.log(i)
-      }
+    return retornoPar
+}
+evensToTwenty(20)
+console.log(evensToTwenty(20))
+
+
+
+
+function oddsToTwenty(numImpar) {
+  let retornoImpar = []
+  for (let i = 1; i <= numImpar; i++) {
+    if (i % 2 !== 0){
+      retornoImpar.push(i)
     }
   }
-  oddNumbersBackwards(1)
-  
-  
-  
-  function multiplesOfFiveBackwards(multDe5Invertido) {
-      
-    for (i = 100; i >= multDe5Invertido; i--) {
-        if (i % 5 == 0) {
-        console.log(i);
+  return retornoImpar
+}
+oddsToTwenty(20)
+console.log(oddsToTwenty(20))
+
+
+
+
+function multiplesOfFive(multiplosDe5) {
+    let multiplo5 = []
+    for (i = 5; i <= multiplosDe5; i++) {
+        if (i % 5 == 0){
+        multiplo5.push(i)
       }
     }
+    return multiplo5
   }
-  multiplesOfFiveBackwards(1) 
-  
-  
-  
-  function squareNumbersBackwards(numQuadradoInvertido) {
-      
-    for (let i = 100; i >= numQuadradoInvertido; i--) {
+multiplesOfFive(100)
+console.log(multiplesOfFive(100))
+
+
+function squareNumbers(numQuadrado) {
+    let quadrado = []
+    for (let i = 1; i <= numQuadrado; i++) {
       if (Math.sqrt(i) % 1 === 0) {
-        console.log(i)
+        quadrado.push(i)
       }
     }
+    return quadrado
+}
+squareNumbers(100)
+console.log(squareNumbers(100))
+
+function countingBackwards(num20a1) {
+   let backNum = [] 
+   for (let i = 20; i >= num20a1; i--){
+    backNum.push(i)
   }
-  squareNumbersBackwards(1)
+  return backNum
+}
+countingBackwards(1)
+console.log(countingBackwards(1))
+
+
+
+function evenNumbersBackwards(parInvertido) {
+  let parBack = []
+  for (let i = 20; i >= parInvertido; i--) {
+    if (i % 2 === 0){
+      parBack.push(i)
+    }
+  }
+  return parBack
+}
+evenNumbersBackwards(1)
+console.log(evenNumbersBackwards(1))
+
+
+function oddNumbersBackwards(imparInvertido) {
+  let imparBack = []    
+  for (let i = 20; i >= imparInvertido; i--) {
+    if (i % 2 !== 0){
+      imparBack.push(i)
+    }
+  }
+  return imparBack
+}
+oddNumbersBackwards(1)
+console.log(oddNumbersBackwards(1))
+
+
+function multiplesOfFiveBackwards(multDe5Invertido) {
+   let multiBack5 = [] 
+  for (i = 100; i >= multDe5Invertido; i--) {
+      if (i % 5 == 0) {
+      multiBack5.push(i);
+    }
+  }
+  return multiBack5
+}
+multiplesOfFiveBackwards(1) 
+console.log(multiplesOfFiveBackwards(1))
+
+
+function squareNumbersBackwards(numQuadradoInvertido) {
+   let quadradoBack = [] 
+  for (let i = 100; i >= numQuadradoInvertido; i--) {
+    if (Math.sqrt(i) % 1 === 0) {
+      quadradoBack.push(i)
+    }
+  }
+  return quadradoBack
+}
+squareNumbersBackwards(1)
+console.log(squareNumbersBackwards(1))
